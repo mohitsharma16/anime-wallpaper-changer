e55343eb7e779ec6e5e649049352022909e62886
mohitsharma404 <mohit.sharma@sedris-vision.com>
2025-10-08 11:54:43 +0530

    feat: implement full Linux support for GNOME, KDE, and XFCE

--- utils/autostart_linux.go
+++ utils/autostart_linux.go
@@ -2,7 +2,57 @@
 
 package utils
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 // SetupAutostart creates a .desktop file in ~/.config/autostart.
 func SetupAutostart() error {
-	return ErrUnsupportedOS
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("failed to get home directory: %w", err)
+	}
+
+	ex, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("failed to get executable path: %w", err)
+	}
+
+	autostartDir := filepath.Join(homeDir, ".config", "autostart")
+	if err := os.MkdirAll(autostartDir, 0755); err != nil {
+		return fmt.Errorf("failed to create autostart directory: %w", err)
+	}
+
+	desktopFilePath := filepath.Join(autostartDir, "wallsync.desktop")
+
+	desktopContent := fmt.Sprintf(`[Desktop Entry]
+Type=Application
+Name=WallSync
+Comment=Automatic wallpaper changer
+Exec=%s -daemon
+Terminal=false
+Hidden=false
+X-GNOME-Autostart-enabled=true
+`, ex)
+
+	if err := os.WriteFile(desktopFilePath, []byte(desktopContent), 0644); err != nil {
+		return fmt.Errorf("failed to write desktop file: %w", err)
+	}
+
+	return nil
+}
+
+// RemoveAutostart removes the .desktop file.
+func RemoveAutostart() error {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("failed to get home directory: %w", err)
+	}
+
+	desktopFilePath := filepath.Join(homeDir, ".config", "autostart", "wallsync.desktop")
+	os.Remove(desktopFilePath)
+
+	return nil
 }
--- utils/utils_linux.go
+++ utils/utils_linux.go
@@ -8,17 +8,40 @@ import (
 
 // SetWallpaper sets the desktop wallpaper on Linux.
 func SetWallpaper(path string) error {
-	// GNOME
+	// Try GNOME first
 	cmd := exec.Command("gsettings", "set", "org.gnome.desktop.background", "picture-uri", "file://"+path)
 	if err := cmd.Run(); err == nil {
 		return nil
 	}
 
-	// KDE
-	// TODO: Implement KDE support
+	// Try KDE Plasma
+	script := `
+		qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript '
+			var allDesktops = desktops();
+			for (i=0;i<allDesktops.length;i++) {
+				d = allDesktops[i];
+				d.wallpaperPlugin = "org.kde.image";
+				d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+				d.writeConfig("Image", "file://` + path + `");
+			}
+		'
+	`
+	cmd = exec.Command("sh", "-c", script)
+	if err := cmd.Run(); err == nil {
+		return nil
+	}
 
-	// XFCE
-	// TODO: Implement XFCE support
+	// Try XFCE
+	cmd = exec.Command("xfconf-query", "-c", "xfce4-desktop", "-p", "/backdrop/screen0/monitor0/workspace0/last-image", "-s", path)
+	if err := cmd.Run(); err == nil {
+		return nil
+	}
 
 	return ErrUnsupportedOS
+}
+
+// ShowNotification displays a notification on Linux using notify-send.
+func ShowNotification(title, message string) {
+	cmd := exec.Command("notify-send", title, message)
+	cmd.Run()
 }
